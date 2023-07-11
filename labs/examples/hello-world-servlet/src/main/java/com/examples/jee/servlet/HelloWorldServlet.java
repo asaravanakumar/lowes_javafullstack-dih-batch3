1d09fd7a4d6063deac03092cd5e9d824fae60ab8
Saravana Kumar <saravana.anbarasu@gmail.com>
2023-07-11 13:24:55 +0530

    hello-world-servlet fix

--- labs/examples/hello-world-servlet/src/main/java/com/examples/jee/servlet/HelloWorldServlet.java
+++ labs/examples/hello-world-servlet/src/main/java/com/examples/jee/servlet/HelloWorldServlet.java
@@ -6,9 +6,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Enumeration;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Hello World Servlet
